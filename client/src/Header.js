47a9f7c62c18188a1f05c257899c37153e88cdc3
Michael Gee <michaelgee221@gmail.com>
2018-02-27 13:40:20 -0500

    Header Styling Changes
    Changed header styles to be taller, well centered, and responsive

--- client/src/Header.js
+++ client/src/Header.js
@@ -3,13 +3,17 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div className="Header">
-        <ul>
-          <li><a href="#">Link1</a></li>
-          <li><a href="#">Link2</a></li>
-          <li><a href="#">Link3</a></li>
-          <li><a href="#">Link4</a></li>
+      <div className="header">
+        <h1 className="header-title">Bears 10 Project</h1>
+
+        <ul className="nav-links">
+          <li><a className="nav-link" href="#">Link1</a></li>
+          <li><a className="nav-link" href="#">Link2</a></li>
+          <li><a className="nav-link" href="#">Link3</a></li>
+          <li><a className="nav-link" href="#">Link4</a></li>
         </ul>
+
+        <div className="menu-btn">BTN</div>
       </div>
     );
   }
