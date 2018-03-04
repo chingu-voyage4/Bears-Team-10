4e9ecbdc5229f49c107a263915f5541f1a4c9bf7
Michael Gee <michaelgee221@gmail.com>
2018-03-03 20:37:53 -0500

    SearchPage implemented - SEARCH & MENU Icons added to the Header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,6 +8,7 @@ import HomePage from './Views/HomePage';
 import MapPage from './Views/MapPage';
 import PictureOfTheDay from './Views/PictureOfTheDay';
 import RedditFeed from './Elements/RedditFeed';
+import SearchPage from './Views/SearchPage';
 import SpaceX from './Views/SpaceX';
 import StackExchange from './Views/StackExchange';
 import './App.css';
@@ -23,6 +24,7 @@ class App extends Component {
             <Route exact path="/" component={HomePage} />
             <Route path="/map" component={MapPage} />
             <Route path="/pictureoftheday" component={PictureOfTheDay} />
+            <Route path="/search" component={SearchPage} />
             <Route path="/spacex" component={SpaceX} />
             <Route path="/stack-exchange" component={StackExchange} />
           </Switch>
