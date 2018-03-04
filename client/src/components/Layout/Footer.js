28a752a742edd9c1b8867d50439fa1cafcd165cf
Michael Gee <michaelgee221@gmail.com>
2018-03-03 19:54:08 -0500

    Footer Styling & Content Changed
    Slightly Changed the footer content to actual links and made styling changes

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
-import Icon from 'react-fa';
+import { Link } from 'react-router-dom';
 
 class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-        <div className="container">
-        <ul className="footer-section">
-          <h3>Links</h3>
-          <li><a href="#">Link1</a></li>
-          <li><a href="#">Link2</a></li>
-          <li><a href="#">Link3</a></li>
-          <li><a href="#">Link4</a></li>
-        </ul>
-        <ul className="footer-section">
-          <h3>Social</h3>
-          <li><a href="#">Github</a></li>
-          <li><a href="#">Github</a></li>
-          <li><a href="#">Github</a></li>
-        </ul>
-        <ul className="footer-section">
-          <h3>Team</h3>
-            <li><a href="https://github.com/michaelgee22" target="_blank">michaelgee22</a></li>
-            <li><a href="https://github.com/Nicknyr" target="_blank">nicknyr</a></li>
-            <li><a href="https://github.com/sam4815" target="_blank">sam4815</a></li>
-            <li><a href="https://github.com/SharpEleven91" target="_blank">sharpeleven11</a></li>
-        </ul>
+        <div className="footer-container">
+          <ul className="footer-section">
+            <h3 className="footer-title">Pages</h3>
+            <li><Link to="/map">Map</Link></li>
+            <li><Link to="/pictureoftheday">POTD</Link></li>
+            <li><Link to="/spacex">SpaceX</Link></li>
+            <li><Link to="/stack-exchange">Stack Exchange</Link></li>
+          </ul>
+          <ul className="footer-section">
+            <h3 className="footer-title">Social</h3>
+            <li><a href="https://github.com/chingu-voyage4/Bears-Team-10" target="_blank">Github</a></li>
+            <li><a href="#">Other Media</a></li>
+            <li><a href="#">Other Media</a></li>
+          </ul>
+          <ul className="footer-section">
+            <h3 className="footer-title">Team</h3>
+              <li><a href="https://github.com/michaelgee22" target="_blank">michaelgee22</a></li>
+              <li><a href="https://github.com/Nicknyr" target="_blank">nicknyr</a></li>
+              <li><a href="https://github.com/sam4815" target="_blank">sam4815</a></li>
+              <li><a href="https://github.com/SharpEleven91" target="_blank">sharpeleven11</a></li>
+          </ul>
         </div>
       </div>
     );
