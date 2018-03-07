2f73059e07d3746ae7e011b2ba3c124b64a0b21f
Nick Kinlen <nick_kinlen@hotmail.com>
2018-03-07 12:51:55 -0500

    Added list of suggested search terms for Searchbar.js

--- client/src/components/Elements/SearchBar.js
+++ client/src/components/Elements/SearchBar.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
 import Autosuggest from 'react-autosuggest';
+import {list} from './SearchSuggestions';
 
-// Imagine you have a list of languages that you'd like to autosuggest.
-const languages = [
-  {
-    name: 'C',
-    year: 1972
-  },
-  {
-    name: 'Elm',
-    year: 2012
-  },
-
-];
+// Importing list const that contains search terms to suggest
 
 // Teach Autosuggest how to calculate suggestions for any given input value.
 const getSuggestions = value => {
   const inputValue = value.trim().toLowerCase();
   const inputLength = inputValue.length;
 
-  return inputLength === 0 ? [] : languages.filter(lang =>
-    lang.name.toLowerCase().slice(0, inputLength) === inputValue
+  return inputLength === 0 ? [] : list.filter(searchTerm =>
+    searchTerm.name.toLowerCase().slice(0, inputLength) === inputValue
   );
 };
 
---
+++ client/src/components/Elements/SearchSuggestions.js
@@ -0,0 +1,160 @@
+
+export const list = [
+  {
+    name: 'Nasa',
+    year: 1972
+  },
+  {
+    name: 'SpaceX',
+  },
+  {
+    name: 'Mercury',
+  },
+  {
+    name: 'Venus',
+  },
+  {
+    name: 'Earth',
+  },
+  {
+    name: 'Mars',
+  },
+  {
+    name: 'Jupiter',
+  },
+  {
+    name: 'Saturn',
+  },
+  {
+    name: 'Uranus',
+  },
+  {
+    name: 'Pluto',
+  },
+  {
+    name: 'Asteroids',
+  },
+  {
+    name: 'Astromony Picture of the Day',
+  },
+  {
+    name: 'Black Holes',
+  },
+  {
+    name: 'Climate',
+  },
+  {
+    name: 'Clouds',
+  },
+  {
+    name: 'Historic Missions',
+  },
+  {
+    name: 'Internation Space Station (ISS)',
+  },
+  {
+    name: 'Johnson Space Center',
+  },
+  {
+    name: 'JPL (Jet Propulsion Laboratory)',
+  },
+  {
+    name: 'Kennedy Space Center',
+  },
+  {
+    name: 'Launches',
+  },
+  {
+    name: 'Lunar Eclipse',
+  },
+  {
+    name: 'Mars Exploration Rovers (Spirit and Opportunity)',
+  },
+  {
+    name: 'Meteors and Meteorites',
+  },
+  {
+    name: 'Milky Way',
+  },
+  {
+    name: 'Moon (Earths Moon)',
+  },
+  {
+    name: 'Moon',
+  },
+  {
+    name: 'NASA History',
+  },
+  {
+    name: 'NOAA',
+  },
+  {
+    name: 'Past Missions',
+  },
+  {
+    name: 'Planets',
+  },
+  {
+    name: 'Robotics',
+  },
+  {
+    name: 'Solar Eclipse',
+  },
+  {
+    name: 'Solar System',
+  },
+  {
+    name: 'Space Shuttle',
+  },
+  {
+    name: 'Space Station',
+  },
+  {
+    name: 'Space Travel',
+  },
+  {
+    name: 'Space Weather',
+  },
+  {
+    name: 'Spacewalks',
+  },
+  {
+    name: 'Stars',
+  },
+  {
+    name: 'Sun',
+  },
+  {
+    name: 'Supernova',
+  },
+  {
+    name: 'Supersonic Flight',
+  },
+  {
+    name: 'Technology',
+  },
+  {
+    name: 'The Big Bang',
+  },
+  {
+    name: 'Universe',
+  },
+  {
+    name: 'Viking',
+  },
+  {
+    name: 'Voyager',
+  },
+  {
+    name: 'Dragon (SpaceX)',
+  },
+  {
+    name: 'Falcon 9 (SpaceX)',
+  },
+  {
+    name: 'Falcon Heavy (SpaceX)',
+  },
+  {
+    name: 'Rockets',
+  },
+];
